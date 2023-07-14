96a2f4b34c4bfcc84b6917e30f42e690efe7ad50
Hanna Vakulenko <33858144+HannaVakulenko@users.noreply.github.com>
2023-07-14 11:28:47 +0300

    Add token error checking: TokenExpiredError and JsonWebTokenError

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -8,24 +8,46 @@ const { SECRET_KEY } = process.env;
 
 const authenticate = async(req, res, next) => {
     const { authorization = "" } = req.headers;
+
+    if (typeof authorization !== "string") {
+        return res.status(401).json({ error:  "No token provided" });
+      }
+
     const [bearer, token] = authorization.split(" ", 2);
     // checking for the "Bearer" in Headers
     if (bearer !== "Bearer") {
         next(HttpError(401, 'Not authorized'));
     }
 
-    // token validation
-    try {
-        const { id } = jwt.verify(token, SECRET_KEY);
-        const user = await User.findById(id);
-        if (!user || !user.token) {
+    jwt.verify(token, SECRET_KEY, async (err, decode) => {
+        // token error checking: "TokenExpiredError" "JsonWebTokenError"
+        if (err) {
+          if (
+            err.name === "TokenExpiredError" ||
+            err.name === "JsonWebTokenError"
+          ) {
+            return res.status(401).json({ error: "Token Error" });
+          }
+    
+          return next(err);
+        }
+    
+        // user token validation
+        try {
+            const { id } = jwt.verify(token, SECRET_KEY);
+            const user = await User.findById(id);
+            if (!user || !user.token) {
+                next(HttpError(401, 'Not authorized'));
+            }
+            req.user = user;
+            next();
+        } catch {
             next(HttpError(401, 'Not authorized'));
         }
-        req.user = user;
-        next();
-    } catch {
-        next(HttpError(401, 'Not authorized'));
-    }
+      });
+
+    
+    
 }
 
 module.exports = authenticate;
